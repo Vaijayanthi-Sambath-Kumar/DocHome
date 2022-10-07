1166a82b7d7768cb24805d8de2db69ec5f33c60c
Reeba Qureshi <64488642+reeba212@users.noreply.github.com>
2022-10-07 17:02:04 +0530

    Update DocHome_Prediction.py

--- DocHome_Prediction.py
+++ DocHome_Prediction.py
@@ -101,13 +101,13 @@ def pred_disease(string):
     # Predict disease
     prediction = mlp.predict_proba([sample_x])
     
-    k = 10
+    k = 5
     diseases = list(set(Y['label_dis']))
     diseases.sort()
     topk = prediction[0].argsort()[-k:][::-1]
     
     topk_dict = {}
-    # Show top 10 highly probable disease to the user.
+    # Show top 5 highly probable disease to the user.
     for idx,t in  enumerate(topk):
         match_sym=set()
         row = df_norm.loc[df_norm['label_dis'] == diseases[t]].values.tolist()
